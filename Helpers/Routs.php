56f2260d6634f0edb8db7630ee1eca40c1040123
Suntechnic <madzhugin@yandex.ru>
2024-02-14 15:53:16 +0300

    Create Routs.php

---
+++ Helpers/Routs.php
@@ -0,0 +1,62 @@
+<?
+declare(strict_types=1);
+
+namespace Bxx\Helpers
+{
+    /*
+    * хелпер для работы с маршрутами
+    */
+    class Routs
+    {
+        
+
+        /**
+         * Возрващает справочник маршрутов проекта относительно текущей позиции
+         */
+        public static function getRelativeMap (): array
+        {
+            $router = \Bitrix\Main\Application::getInstance()->getRouter();
+            $app = \Bitrix\Main\Application::getInstance(); 
+            $routeCurrent = $app->getCurrentRoute();
+            $UriRouteCurrent = $routeCurrent->getUri();
+            $ulUriRouteCurrent = explode('/',$UriRouteCurrent);
+            
+            $refRouteMap = [];
+            
+            foreach ($router->getRoutes() as $route) {
+                $UriRoute = $route->getUri();
+                $ulUriRoute = explode('/',$UriRoute);
+                foreach ($ulUriRoute as $TokenI=>$Token) {
+                    if (isset($ulUriRouteCurrent[$TokenI])) {
+                        if ($Token == $ulUriRouteCurrent[$TokenI]) {
+                            $refRouteMap[$TokenI][] = [
+                                    'route' => $route,
+                                    'Uri' => $UriRoute
+                                ];
+                        } else break;
+                    } else break;
+                }
+            }
+            return $refRouteMap;
+        }
+
+        /**
+         * Возрващает справочник маршрутов проекта относительно текущей позиции
+         */
+        public static function isCurrent (\Bitrix\Main\Routing\Route $route): bool
+        {
+            $optionsRoute = $route->getOptions();
+            //if ($optionsRoute->hasName()) {
+                $router = \Bitrix\Main\Application::getInstance()->getRouter();
+                $app = \Bitrix\Main\Application::getInstance(); 
+                $routeCurrent = $app->getCurrentRoute();
+                if ($route->getUri() == $routeCurrent->getUri()) {
+                    // $optionsRouteCurrent = $routeCurrent->getOptions();
+                    // if ($optionsRoute->getFullName() == $optionsRouteCurrent->getFullName()) 
+                            return true;
+                }
+            //}
+            return false;
+        }
+    }
+}
