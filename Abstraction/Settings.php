19d285ea955790520beadf89d7cd23697e4130f8
Александр Маджугин <madzhugin@gmail.com>
2024-11-13 09:20:40 +0000

    поддержка настройки путей api

--- Abstraction/Settings.php
+++ Abstraction/Settings.php
@@ -4,7 +4,41 @@ namespace Bxx\Abstraction
     class Settings
     {
         public const MODULE = '.app';
-        public const OPTIONS = [];
+        // может содержать список опций приложения, описанные здесь опции попадут в ключ options конфигурации
+        public const OPTIONS = [/*
+                'OptionName' => [
+                        'title' => 'Название опции для отображения в админке',
+                        'default' => 'ЗначениеПоУмолчанию'
+                    ]
+            */]; 
+        // может содержать дополнительные ключи для массива конфиг, 
+        // где значения - это имена методово реализации этого класса
+        protected const KEYS = [/*
+                'Key' => 'MethodName'
+            */];
+        /**
+         * Пример метода загрузки путей api
+        public static function getApi (): array
+        {
+            $refApi = [];
+            $router = \Bitrix\Main\Application::getInstance()->getRouter();
+            foreach ($router->getRoutes() as $route) {
+                $options = $route->getOptions();
+                if ($options->getFullPrefix() == '/api/v1' // это api
+                        && $options->getFullName() // и он именованный (публичный)
+                    ) {
+                    $refApi[$options->getFullName()] = [
+                            'uri' => $route->getUri(),
+                            'parameters' => $route->getParameters(),
+                            'methods' => $options->getMethods()
+                        ];
+                }
+                
+            }
+            return $refApi;
+        }
+         */
+
         public const CACHE_TTL = 86399;
         public const CACHE_TTL_LIMIT = 86399;
         
@@ -12,23 +46,41 @@ namespace Bxx\Abstraction
         {
             return $code;
         }
-        
-        public static function getOption (string $code)
+
+
+        /**
+         * возвращает массив Опция=>Значение
+         * для опций описанных в константе OPTIONS
+         */
+        public static function getOptions (): array
         {
-            if (static::OPTIONS[$code]) $default = static::OPTIONS[$code]['default'];
+            $dctOptions = [];
+            foreach (static::OPTIONS as $Code=>$_) {
+                $dctOptions[$Code] = static::getOption($Code);
+            }
+            return $dctOptions;
+        }
+        
+        /**
+         * возвращает знаение опции приложения по его имени
+         * если необходимо дефолтное значение - оно должно быть описано в константе OPTIONS
+         */
+        public static function getOption (string $Code)
+        {
+            if (static::OPTIONS[$Code]) $default = static::OPTIONS[$Code]['default'];
             return \Bitrix\Main\Config\Option::get(
                     static::MODULE,
-                    static::getOptionKey($code),
+                    static::getOptionKey($Code),
                     $default
                 );
         }
         
-        public static function setOption (string $code, $value)
+        public static function setOption (string $Code, $Value)
         {
             return \Bitrix\Main\Config\Option::set(
                     static::MODULE,
-                    static::getOptionKey($code),
-                    $value
+                    static::getOptionKey($Code),
+                    $Value
                 );
         }
         
@@ -46,10 +98,9 @@ namespace Bxx\Abstraction
         {
             if (!$timestamp) $timestamp = $_SERVER['REQUEST_TIME']; // если время не передано
                                                                     // наивно предположим что оно такое же как на сервере
+
             $arConfig = [
-                    'options' => [
-                            
-                        ],
+                    'options' => static::getOptions(),
                     'env' => [
                             'stage' => defined('APPLICATION_ENV')?APPLICATION_ENV:'production',
                             'timestamp' => time(),
@@ -61,7 +112,7 @@ namespace Bxx\Abstraction
                     'bitrix' => [
                             'SITE_TEMPLATE_PATH' => SITE_TEMPLATE_PATH,
                             'START_EXEC_TIME' => START_EXEC_TIME,
-                        ]
+                        ],
                 ];
             
             global $USER;
@@ -78,6 +129,11 @@ namespace Bxx\Abstraction
                         'id' => 0,
                         'email_hash' => ''
                     ];
+            }
+
+            // дополнительные ключи
+            foreach (static::KEYS as $Key=>$Method) {
+                $arConfig[$Key] = static::$Method();
             }
             
             return $arConfig;
