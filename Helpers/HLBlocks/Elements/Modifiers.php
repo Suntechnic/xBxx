5b05f8807e980025d3f53509a13526d6c395efa8
Александр Маджугин <madzhugin@gmail.com>
2025-06-04 19:42:33 +0000

    добаление Helpers/HLBlocks

---
+++ Helpers/HLBlocks/Elements/Modifiers.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Bxx\Helpers\HLBlocks\Elements
+{   /**
+    * коллекция модифкаторов для массивов элементов хайлойдблоков
+    */
+    class Modifiers
+    {
+
+    }
+
+}
