1fe29cacc8a606cb52b6d76436fec70e4b524e68
sohoshop.ru <info@sohoshop.ru>
2025-10-26 11:36:21 +0300

    исправление пробелмы цекличного вызова

--- Settings.php
+++ Settings.php
@@ -5,10 +5,15 @@ namespace Bxx
     {
         public static function getCacheTTL (int $TTL=0): int
         {
+            // Проверяем, что метод переопределен в App\Settings
             if (class_exists('\App\Settings')) {
-                return \App\Settings::getCacheTTL();
+                $reflection = new \ReflectionMethod('\App\Settings', 'getCacheTTL');
+                // Если метод определен именно в App\Settings (не унаследован)
+                if ($reflection->getDeclaringClass()->getName() === 'App\Settings') {
+                    return \App\Settings::getCacheTTL($TTL);
+                }
             }
-            return parent::getCacheTTL();
+            return parent::getCacheTTL($TTL);
         }
     }
 }
