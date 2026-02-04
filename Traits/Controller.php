19f3fb1423c0f910098b9dc3809b982909cf55f0
RETAIL-PROD <retail@iprofi.ru>
2026-02-04 13:44:29 +0300

    поддержка классов prefilters строкой

--- Traits/Controller.php
+++ Traits/Controller.php
@@ -7,7 +7,20 @@ namespace Bxx\Traits {
         protected function init()
         {
             parent::init();
-            foreach ($this->actionsConfig as $name=>$arConfig) $this->setActionConfig($name, $arConfig);
+            foreach ($this->actionsConfig as $Name=>$refConfig) {
+                
+                foreach ($refConfig as $NameFilter=>$lstConfig) {
+                    if ($NameFilter == 'prefilters') {
+                        foreach ($lstConfig as $I=>$Filter) {
+                            if (is_string($Filter)) {
+                                $refConfig[$NameFilter][$I] = new $Filter;
+                            }
+                        }
+                    }
+                }
+                
+                $this->setActionConfig($Name, $refConfig);
+            }
         }
     }
 }
