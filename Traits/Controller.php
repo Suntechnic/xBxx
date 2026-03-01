f226c9f59b4107a13699e116cb0b9c113d6143d4
Александр Маджугин <madzhugin@gmail.com>
2026-03-01 11:41:35 +0000

    в режиме разработки всегда добавляем -prefilter с '\Bitrix\Main\Engine\ActionFilter\Csrf' в контроллерах

--- Traits/Controller.php
+++ Traits/Controller.php
@@ -7,6 +7,18 @@ namespace Bxx\Traits {
         {
             parent::init();
             foreach ($this->actionsConfig as $name=>$arConfig) {
+                
+                if (defined('APPLICATION_ENV') && APPLICATION_ENV == 'dev') {
+                    // в режиме разработки всегда добавляем -prefilter с '\Bitrix\Main\Engine\ActionFilter\Csrf'
+                    if (!isset($arConfig['-prefilters'])) {
+                        $arConfig['-prefilters'] = ['\Bitrix\Main\Engine\ActionFilter\Csrf'];
+                    } elseif (!in_array('\Bitrix\Main\Engine\ActionFilter\Csrf', $arConfig['-prefilters'])) {
+                        $arConfig['-prefilters'][] = '\Bitrix\Main\Engine\ActionFilter\Csrf';
+                    }
+                }
+
+
+                // преобразование строковых классов в объекты
                 if ($arConfig['prefilters']) {
                     foreach ($arConfig['prefilters'] as $I=>$FilterClass) {
                         if (is_string($FilterClass)) {
